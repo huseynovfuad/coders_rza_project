0a2e09c5cb5ca2e6b4a11a0a81ca56ade3fbefc6
Fuad Huseynov <fhuseynov803@gmail.com>
2023-04-17 20:16:22 +0400

    reverse function deleted

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category, Basket
 from django.db.models import F, FloatField, Q, Sum
 from django.db.models.functions import Coalesce
